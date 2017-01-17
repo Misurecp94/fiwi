2dc72d155487e60f3c0204398df74ede69a2f4c3
misurecp94 <a1325267@unet.univie.ac.at>
2017-01-17 15:07:39 +0100

    Some things

--- MonteCarlo/normalDistGenerator.py
+++ MonteCarlo/normalDistGenerator.py
@@ -2,7 +2,8 @@
 import numpy as np
 
 # Calculates "anzahl" random numbers from the normal distribution N(erwartungswert,varianz)
-#
+
 
 def calcrandomnumbers(erwartungswert, varianz, anzahl):
+
     return np.random.normal(erwartungswert, varianz, anzahl)
--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 # should ultimately display the GUI and calculate everything where the user can change the following aspects:
 # number of days to keep the stock, starting price, anual volatility, number of monteCarlo iterations
-# should also draw afterwards a chart (results of each iteration, iteration over all days = 1 iteration, so do it x time
+# should also draw afterwards a chart (last results of each iteration, iteration over all days = 1 iteration,
+# so do it x time
 # should also calculate mean, median, standard derivation, and the 5%,25% and 95% percentile of the result
 # should also calculate the calc. val at risk and val at risk
 
-# val at risk = x% chance y% oder mehr von Portfolio verlieren in einem Tag!
-# calc val at risk = in the worst x% of returns the average loss will be y%
+# val at risk (VAR) = x% chance y% oder mehr von Portfolio verlieren in einem Tag!
+# x% VAL = (1-(1-x))*total Count of Returns bei sortiertem aufliegen (=x% Quantil!!!)
+# calc val at risk (CVAL) = in the worst x% of returns the average loss will be y%
+# x% CVAL = (1/VAR x%)*sum(from worst return to value of VAR x%)
+
+
+# In one iteration do for each step:
+# Simulated Price of that day = Starting price from the day before * (1+random number from N(0,x)
+# Erwartungswert standard = 0 (user könnte ändern!) wegen der Random Walk Theory
+# Last day = Result from the first iteration
+
+# Calculate everything from there last days
 
 # Todo: implement methods to reduce the varianz
 
