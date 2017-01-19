4f37a77d9a9965f3db18bcaa552310b96ce6a066
Lukas <Lukas.Girsch@gmail.com>
2017-01-19 19:50:08 +0100

    line chart hinzugefügt layout verändert download von aktienkursen

--- main.py
+++ main.py
@@ -25,14 +25,42 @@ from os.path import dirname, join
 
 import numpy as np
 import pandas.io.sql as psql
+import pandas as pd
 import sqlite3 as sql
+import os
 
 from bokeh.plotting import figure
-from bokeh.layouts import layout, widgetbox
+from bokeh.layouts import layout, widgetbox, row, column
+
 from bokeh.models import ColumnDataSource, HoverTool, Div
-from bokeh.models.widgets import Slider, Select, TextInput
+from bokeh.models.widgets import PreText, Slider, Select, TextInput
 from bokeh.io import curdoc
 from bokeh.sampledata.movies_data import movie_path
+
+
+try:
+    from functools import lru_cache
+except ImportError:
+    # Python 2 does stdlib does not have lru_cache so let's just
+    # create a dummy decorator to avoid crashing
+    print ("WARNING: Cache is available on Python 3 only.")
+
+    def lru_cache():
+        def dec(f):
+            def _(*args, **kws):
+                return f(*args, **kws)
+            return _
+        return dec
+
+
+DATA_DIR = join(dirname(__file__), 'daily')
+
+DEFAULT_TICKERS = ['AAPL', 'GOOG', 'INTC', 'BRCM', 'YHOO']
+
+# (not in x) gibt eine liste zurück in der val nicht enthalten ist
+def nix(val, lst):
+    return [x for x in lst if x != val]
+
 
 # WE DONT NEED A DATABASE ANYWAY
 conn = sql.connect(movie_path)
@@ -58,27 +86,25 @@ axis_map = {
     "Year": "Year",
 }
 
+# Html Beschreibung
 desc = Div(text=open(join(dirname(__file__), "Html/description.html")).read(), width=800)
 
-# Create Input controls Todo: Add Method which automaticaley chooses option based on available csv files??? MAYBE!
-stock=select = Select(title="Stock:", value="Google", options=["Google", "Apple", "Microsoft"])
+# Create Input controls Todo: Add Method which automaticaley chooses option based on available csv files??? DONE!
+stock_ticker=os.listdir("CSV") # returns list
+stock = Select(title="Stock:", value="Google", options=stock_ticker)
+
+
 days = Slider(title="Number of days to keep the stock", value=100, start=1, end=252, step=1)
 
 # Non used
 min_year = Slider(title="Year released", start=1940, end=2014, value=1970, step=1)
-max_year = Slider(title="End Year released", start=1940, end=2014, value=2014, step=1)
-oscars = Slider(title="Minimum number of Oscar wins", start=0, end=4, value=0, step=1)
-boxoffice = Slider(title="Dollars at Box Office (millions)", start=0, end=800, value=0, step=1)
-genre = Select(title="Genre", value="All",
-               options=open(join(dirname(__file__), 'Txt/genres.txt')).read().split())
 director = TextInput(title="Director name contains")
-cast = TextInput(title="Cast names contains")
 x_axis = Select(title="X Axis", options=sorted(axis_map.keys()), value="Tomato Meter")
 y_axis = Select(title="Y Axis", options=sorted(axis_map.keys()), value="Number of days")
 
 
 # Create Column Data Source that will be used by the plot
-source = ColumnDataSource(data=dict(x=[], y=[], color=[], title=[], year=[], revenue=[], alpha=[]))
+source = ColumnDataSource(data=dict(date=[], o=[], h=[], l=[], c=[], volume=[]))
 
 hover = HoverTool(tooltips=[
     ("Title", "@title"),
@@ -89,71 +115,98 @@ hover = HoverTool(tooltips=[
 p = figure(plot_height=600, plot_width=700, title="", toolbar_location=None, tools=[hover])
 p.circle(x="x", y="y", source=source, size=7, color="color", line_color=None, fill_alpha="alpha")
 
-# HIER: BERECHNE DATEN
-def select_movies():
-    genre_val = genre.value
-    director_val = director.value.strip()
-    cast_val = cast.value.strip()
-    selected = movies[
-        (movies.Reviews >= days.value) &
-        (movies.BoxOffice >= (boxoffice.value * 1e6)) &
-        (movies.Year >= min_year.value) &
-        (movies.Year <= max_year.value) &
-        (movies.Oscars >= oscars.value)
-    ]
-    if genre_val != "All":
-        selected = selected[selected.Genre.str.contains(genre_val)]
-    if director_val != "":
-        selected = selected[selected.Director.str.contains(director_val)]
-    if cast_val != "":
-        selected = selected[selected.Cast.str.contains(cast_val)]
-    return selected
 
-# HIER: SETZE UPDATES UND RUFE UPDATEDROPDOWN AUF
+# HIER: LADE DATEN AUS .CSV
+@lru_cache()
+def load_ticker(ticker):
+    fname = join(DATA_DIR, 'table_%s.csv' % ticker.lower())
+    data = pd.read_csv(fname, header=None, parse_dates=['date'],
+                       names=['date', 'foo', 'o', 'h', 'l', 'c', 'v'])
+    data = data.set_index('date')
+    # print(data.c.diff())
+
+    # gibt tabelle auf in format ->     date - ticker(close) - ticker_returns
+    return pd.DataFrame({ticker: data.c, ticker+'_returns': data.c.diff()})
 
 
-def update():
-    df = select_movies()
-    x_name = axis_map[x_axis.value]
-    y_name = axis_map[y_axis.value]
+@lru_cache()
+def get_data(t1):
+    df1 = load_ticker(t1)
+    data = pd.concat([df1], axis=1)
 
-    p.xaxis.axis_label = x_axis.value
-    p.yaxis.axis_label = y_axis.value
-    p.title.text = "%d movies selected" % len(df)
-    source.data = dict(
-        x=df[x_name],
-        y=df[y_name],
-        color=df["color"],
-        title=df["Title"],
-        year=df["Year"],
-        revenue=df["revenue"],
-        alpha=df["alpha"],
-    )
+    data = data.dropna()
+    data['t1'] = data[t1]
+    data['t1_returns'] = data[t1+'_returns']
+    return data
 
-# HIER: BERECHNE DATEN
+# set up widgets
+
+stats = PreText(text='', width=500)
+ticker1 = Select(title='Auswahl der Aktie:', value='AAPL', options=nix('AAPL', DEFAULT_TICKERS))
+select_varianzred = Select(title='Methoden zur Varianzreduktion: ', value='Methode 1', options=['Methode 1', 'Methode 2'])
+
+# set up plots
+
+source_static = ColumnDataSource(data=dict(date=[], t1=[], t1_returns=[]))
+tools = 'pan,wheel_zoom,xbox_select,reset'
+
+
+ts1 = figure(plot_width=900, plot_height=200, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
+ts1.line('date', 't1', source=source_static)
+ts1.circle('date', 't1', size=1, source=source, color=None, selection_color="orange")
+
+
+# set up callbacks
+
+def ticker1_change(attrname, old, new):
+    ticker1.options = nix(new, DEFAULT_TICKERS)
+    update()
+
+
+# wird ausgeführt wenn noch keine auswahl in der selection box getroffen wurde (beim ersten start) - default AAPL
+def update(selected=None):
+    t1 = ticker1.value
+
+    data = get_data(t1)
+    source.data = source.from_df(data[['t1', 't1_returns', ]])
+    source_static.data = source.data
+
+    update_stats(data, t1)
+
+    ts1.title.text, ts1
+
+
+def update_stats(data, t1):
+    stats.text = str(data[[t1, t1+'_returns']].describe())
+
+ticker1.on_change('value', ticker1_change)
+
+
+def selection_change(attrname, old, new):
+    t1 = ticker1.value
+    data = get_data(t1)
+    selected = source.selected['1d']['indices']
+    if selected:
+        data = data.iloc[selected, :]
+    update_stats(data, t1)
 
 
 def updateDropDown():
-    print("geht")
+    print("geht");
 
-controls = [days, boxoffice, genre, min_year, max_year, oscars, director, cast, x_axis, y_axis]
-for control in controls:
-    control.on_change('value', lambda attr, old, new: update())
+source.on_change('selected', selection_change)
 
-dropDowns = [stock]
-for dropDown in dropDowns:
-    dropDown.on_change('value', lambda attr, old, new: updateDropDown())
+# set up layout
+c1 = column(ticker1, select_varianzred)
+c2 = column(stats, ts1)
+r1 = row(c1, c2)
+layout = column(desc, r1)
 
-sizing_mode = 'scale_width'
 
-inputs = widgetbox(*dropDowns, *controls, sizing_mode=sizing_mode)
-l = layout([
-    [desc],
-    [inputs, p],
-], sizing_mode=sizing_mode)
+#layout = column(main_row, series)
 
 update()  # initial load of the data - first load metadata
-updateDropDown()  # initial load of the dropdown - load Stock and calculate results based on metadata
+#updateDropDown()  # initial load of the dropdown - load Stock and calculate results based on metadata
 
-curdoc().add_root(l)
+curdoc().add_root(layout)
 curdoc().title = "Monte Carlo Simulation"
